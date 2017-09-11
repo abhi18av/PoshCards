17b2575b7b4c04fefd580f818dfdac3a0a4cf2c0
Abhinav Sharma <abhi18av@gmail.com>
2017-09-11 13:03:37 +0530

    added a reference for regular expressions

--- 1_sorting/scratch.ps1
+++ 1_sorting/scratch.ps1
@@ -4,3 +4,54 @@ Get-ChildItem ./1_sorting -Hidden
 
 
 get-help gci -System
+
+
+Get-ChildItem  ./1_sorting -Include *.txt -Exclude H*
+Get-ChildItem  ./1_sorting -Hidden  -Exclude *-.txt 
+
+
+
+
+
+<# 
+Regular Expressions
+
+ #>
+
+ "Microsoft" –match "soft"
+ "Software" –match "soft"
+ "Computers" –match "soft"
+
+ "Software" –cmatch "soft"
+
+
+ "Don" –match "D.n" 
+ "Dn" –match "D.n" 
+ "Don" –match "D?n" 
+ "Dn" –match "D?n" 
+
+
+
+
+ "DoDon" –match "Do*n" 
+ "Dn" -match "Do*n" 
+ "DoDon" -match "Do+n" 
+ "Dn" -match "Do+n" 
+
+
+ "D.n" -match "D\.n"
+
+
+ "Shell" -match "\w" 
+ "Shell" -match "\w*" 
+ 
+
+ "Jeff" -match "J[aeiou]ff" 
+ "Jeeeeeeeeeeff" -match "J[aeiou]ff" 
+
+
+ "192.168.15.20" -match "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+
+
+ "\\Server2\Share" -match "\\\\\w+\\\w+"
