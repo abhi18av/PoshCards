834c35fd4e73e3a10c22ffba964077994fb0ac31
Abhinav Sharma <abhi18av@gmail.com>
2017-09-11 13:08:13 +0530

    removed extraneous cases from regexp

--- 1_sorting/scratch.ps1
+++ 1_sorting/scratch.ps1
@@ -18,37 +18,22 @@ Regular Expressions
 
  #>
 
- "Microsoft" –match "soft"
  "Software" –match "soft"
- "Computers" –match "soft"
 
  "Software" –cmatch "soft"
 
 
  "Don" –match "D.n" 
- "Dn" –match "D.n" 
  "Don" –match "D?n" 
- "Dn" –match "D?n" 
-
-
-
 
  "DoDon" –match "Do*n" 
- "Dn" -match "Do*n" 
  "DoDon" -match "Do+n" 
- "Dn" -match "Do+n" 
-
-
- "D.n" -match "D\.n"
-
 
  "Shell" -match "\w" 
  "Shell" -match "\w*" 
  
-
  "Jeff" -match "J[aeiou]ff" 
  "Jeeeeeeeeeeff" -match "J[aeiou]ff" 
-
 
  "192.168.15.20" -match "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
