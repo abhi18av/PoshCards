f0b3430ecb32b5000829c121a941a4ac305561d7
Abhinav Sharma <abhi18av@gmail.com>
2017-09-11 15:29:19 +0530

    working on reading the input from the console

--- 1_sorting/scratch_sorting.ps1
+++ 1_sorting/scratch_sorting.ps1
@@ -2,10 +2,21 @@
 
 Get-ChildItem ./1_sorting -Hidden 
 
-Get-ChildItem  ./1_sorting -Include *.txt -Exclude H*
-
-Get-ChildItem  ./1_sorting -Hidden -Include "*deck*"
+Get-ChildItem  ./1_sorting/ -Include *.txt 
 
 
+Get-ChildItem ./1_sorting -Attributes
+
+Get-ChildItem  ./1_sorting -Exclude *.ps1 
+
+Write-Output "hello" ", world!"  $args
+
+Get-Help Get-FileHash -Examples
 
 
+get-help Add-Type -Examples
+
+
+$x = Read-Host "> enter the x!"
+
+Write-Output $x
